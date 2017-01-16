755b85d30d374fdc9b05bc2de922debca8e14d7c
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-01-16 14:00:58 +0100

    Revert "Point (#2)"

--- src/point.c
+++ src/point.c
@@ -1,9 +1,10 @@
 /* Copyright 2017 Marc Volker Dickmann */
 #include "point.h"
 
-struct _point point_init (const int x, const int y)
+void point_init (struct _point *self, const int x, const int y)
 {
-	return (struct _point) {x, y};
+	self->x = x;
+	self->y = y;
 }
 
 struct _point point_add (const struct _point *a, const struct _point *b)

59d011eb2aff461f0dd5f9a64d661450b238c6cd
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-01-17 13:04:10 +0100

    Update vector.c
    Fix.

--- src/vector.c
+++ src/vector.c
@@ -9,20 +9,20 @@ struct _vector vector_init (const int x, const int y, const int z)
 
 struct _vector vector_add (const struct _vector *a, const struct _vector *b)
 {
-	return (struct _vector) {point_add (a->parent, b->parent), a->z + b->z};
+	return (struct _vector) {point_add (&a->parent, &b->parent), a->z + b->z};
 }
 
 struct _vector vector_sub (const struct _vector *a, const struct _vector *b)
 {
-	return (struct _vector) {point_sub (a->parent, b->parent), a->z - b->z};
+	return (struct _vector) {point_sub (&a->parent, &b->parent), a->z - b->z};
 }
 
 struct _vector vector_mpl (const struct _vector *a, const struct _vector *b)
 {
-	return (struct _vector) {point_mpl (a->parent, b->parent), a->z * b->z};
+	return (struct _vector) {point_mpl (&a->parent, &b->parent), a->z * b->z};
 }
 
 struct _vector vector_div (const struct _vector *a, const struct _vector *b)
 {
-	return (struct _vector) {point_div (a->parent, b->parent), a->z / b->z};
+	return (struct _vector) {point_div (&a->parent, &b->parent), a->z / b->z};
 }
